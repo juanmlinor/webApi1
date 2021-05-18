fcd7c413da39464a2094b4404f3e6da85b611242
jumalire55 <jumalire55@outlook.com>
2021-05-18 13:29:57 -0500

    Video2

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Repositories;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        // private readonly IPostService _postService;
+        // private readonly IMapper _mapper;
+        // private readonly IUriService _uriService;
+        private readonly IPostRepository _postRepository;
+
+        public PostController(IPostRepository postRepository )
+        {
+            _postRepository = postRepository;
+            //_postService = postService;
+            // _mapper = mapper;
+            // _uriService = uriService;
+        }
+
+        public IPostRepository PostRepository { get; }
+
+        public async Task<IActionResult> GetPosts()
+        {
+            var posts = await _postRepository.GetPosts();
+            return Ok(posts);
+        }
+    }
+}
